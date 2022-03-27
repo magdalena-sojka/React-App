a54fd526b97119f93a903b91b02ab7c61840ec40
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-03-27 20:16:46 +0200

    Add components Card and CardForm

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,14 @@
 import styles from './Column.module.scss';
+import Card from '../Card/Card.js';
+import CardForm from '../CardForm/CardForm.js';
 
 const Column = propos => {
   return (<article className={styles.column}>
               <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + propos.icon} />{propos.title}</h2>
+              <ul className={styles.cards}>
+                {propos.cards.map(card => <Card key={card.id} title={card.title} />)}
+              </ul>
+              <CardForm columnId={propos.id} action={propos.action} />
             </article>);
 }
 
