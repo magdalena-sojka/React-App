9860e72baf38278f169c77559531622d7478f4ab
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-04-10 21:08:18 +0200

    Add action to reducer (Add Column & Add Card)

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,14 +2,20 @@ import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const CardForm = props => {
     const [title, setTitle] = useState('');
+
     const handleSubmit = e => {
-        e.preventDefault();
-        props.action({ title: title }, props.columnId);
-        setTitle('');
+      e.preventDefault();
+      dispatch({type: 'ADD_CARD', payload:{ title, columnId: props.columnId }});
+      setTitle('');
     };
+
+
+
+  const dispatch = useDispatch();
 
 	return (
         <form className={styles.cardForm} onSubmit={handleSubmit}>
--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,6 +2,7 @@ import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const ColumnForm = props => {
   const [title, setTitle] = useState('');
@@ -9,10 +10,12 @@ const ColumnForm = props => {
   
   const handleSubmit = e => {
     e.preventDefault();
-    props.action({ title: title, icon: icon });
+    dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
     setTitle('');
     setIcon('');
-};
+ };
+
+  const dispatch = useDispatch();
 
 	return (
     <form onSubmit={handleSubmit} className={styles.columnForm}>
